13dae32f14ec4da7ea9f48595953e601806528b5
Alec W Greene <alecwgreene@gmail.com>
2020-11-21 15:49:41 -0500

    Works now

--- portfolio/src/components/DemosNavBar/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function DemosNavBar(props){
-    return(
-    <div className="navbar navbar-dark navbar-expand-lg">
-    </div>
-    );
-}
--- portfolio/src/pages/Demos/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Demos(props){
-    return <div>Demos placeholder</div>
-}
