81c9407a2546cd5a3b8190c0e406405b45e8ea22
Alec W Greene <alecwgreene@gmail.com>
2024-03-02 13:38:30 -0800

    Update Resume link to new resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Resume(){
-    return <iframe src={"https://drive.google.com/file/d/1HF8a9SV1PlUHIL7wSlXnKdrYu4m7fQzE/view?usp=sharing"}>
+    return <iframe src={"https://docs.google.com/document/d/e/2PACX-1vSZLFm6QF6bzMrK3IwCnaiikghNj6TYlU5FL6J-z70vd1kerkWOQp_tpGXiSdmm1pKLignS0Ub37dv8/pub"}>
 
     </iframe>
 }
